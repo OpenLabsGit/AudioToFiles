67af320801aca0bac65b3873201044de59108b79
OpenLabsGit <graphiste.gwada@gmail.com>
2023-06-28 16:29:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ downloadBtn.addEventListener('click', async () => {
         <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
       </svg>
         </div>
-        <audio src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-61905/zapsplat_multimedia_alert_chime_short_musical_notification_cute_child_like_001_64918.mp3?_=1"></audio>
+        <audio src="${window.URL.createObjectURL(response)}"></audio>
         <div class="controls">
           <button class="player-button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#3D3132">
@@ -126,8 +126,6 @@ downloadBtn.addEventListener('click', async () => {
           </button>
         </div>
       </div>`
-        audioElement.controls = true;
-        audioElement.src = window.URL.createObjectURL(response);
         audioElement.addEventListener('loadeddata', () => {
           loadingIndicator.style.display = 'none';
         });
