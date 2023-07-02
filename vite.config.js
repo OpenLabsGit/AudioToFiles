6abcfc414c6c67722832eb1182eb4b2e3530fced
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-02 17:13:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 const mode = process.env.MODE || 'development';
 const env = loadEnv(mode, process.cwd());
