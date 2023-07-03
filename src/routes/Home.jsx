b3387e40a6d7fcb5bd30be9cd00fde0f35d786e9
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-03 13:27:31 -0400

    Add AOS

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SignedOut, RedirectToSignIn, useUser } from '@clerk/clerk-react';
+import AOS from 'aos';
 
 const Home = () => {
-
   const { isLoaded, isSignedIn, user } = useUser();
+
+  useEffect(() => {
+    AOS.init(); // Initialisation de AOS
+  }, []);
 
   if (!isLoaded || !isSignedIn) {
     return null;
@@ -18,12 +22,15 @@ const Home = () => {
         <h1 className="text-3xl font-bold text-black">Téléchargement de musique {user.firstName}</h1>
       </div>
       <div className="mt-8">
-        <p>Bienvenue sur notre plateforme de téléchargement de musique.</p>
-        <p>Allez dans dashboard accéder à toutes les fonctionnalités.</p>
-        <p>Site actuellement en mise a jour ...</p>
+        <p data-aos="fade-up">Bienvenue sur notre plateforme de téléchargement de musique.</p>
+        <p data-aos="fade-up" data-aos-delay="200">Allez dans le dashboard pour accéder à toutes les fonctionnalités.</p>
+        <p data-aos="fade-up" data-aos-delay="400">Site actuellement en mise à jour...</p>
       </div>
     </>
   );
 };
+
+export default Home;
+
 
 export default Home;
