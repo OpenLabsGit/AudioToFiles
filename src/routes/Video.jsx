8f3d7200c4f86e39385221aa045968783ff042ea
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-03 09:54:05 -0400

    Change Api Key

--- src/routes/Video.jsx
+++ src/routes/Video.jsx
@@ -14,11 +14,11 @@ const Video = () => {
   };
 
   const searchVideos = (keywords) => {
-    const apiKey = "VOTRE_CLE_API_YOUTUBE";
+    const apiKey = "AIzaSyDs9IaDQnj1z_OJUQ0Qd7nONvGQzDN-AP8";
 
     axios
       .get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=5&q=${keywords}&key=${apiKey}`
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${keywords}&key=${apiKey}`
       )
       .then((response) => {
         const videos = response.data.items;
