b3893576dc38d434a40e7712f6f10e2e60e2a717
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-03 09:34:34 -0400

    Change Link

--- src/routes/Video.jsx
+++ src/routes/Video.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios  from "axios";
+import { Link } from "react-router-dom";
 
 const Video = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -37,8 +38,8 @@ const Video = () => {
       <nav className="flex ml-5" aria-label="Breadcrumb">
         <ol className="inline-flex items-center space-x-1 md:space-x-3">
           <li className="inline-flex items-center">
-            <a
-              href="/dashboard"
+            <Link
+              to={"/dashboard"}
               className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
             >
               <svg
@@ -51,7 +52,7 @@ const Video = () => {
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
               </svg>
               Dashboard
-            </a>
+            </Link>
           </li>
 
           <li aria-current="page">
