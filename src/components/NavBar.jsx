ca91d63495ef56117b44b68415576adcc01f6f60
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-03 08:50:10 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,7 +46,7 @@ const Navbars = () => {
             <UserButton />
           ) : (
             <a
-              href="/sign-in"
+              href="https://accounts.audiotofiles.com/sign-in"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               <useSignIn />
