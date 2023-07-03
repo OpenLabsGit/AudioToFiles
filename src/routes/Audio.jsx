b4a463795ac1127e4685e960ff1b0750f2c2477b
OpenLabsGit <graphiste.gwada@gmail.com>
2023-07-03 13:49:57 -0400

    Update Audio.jsx

--- src/routes/Audio.jsx
+++ src/routes/Audio.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios  from "axios";
+import axios from "axios";
 
 const Audio = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,58 +15,55 @@ const Audio = () => {
 
   const searchVideos = (keywords) => {
     const apiKey = "AIzaSyDs9IaDQnj1z_OJUQ0Qd7nONvGQzDN-AP8";
-
     axios
       .get(
         `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${keywords}&key=${apiKey}`
       )
       .then((response) => {
         const videos = response.data.items;
-        const thumbnail = document.querySelector('.thumbnail')
-        const titleHtml = document.querySelector('.title')
-        
+        const thumbnail = document.querySelector(".thumbnail");
+        const titleHtml = document.querySelector(".title");
+
         videos.forEach((video) => {
           const { title, description, thumbnails } = video.snippet;
           const videoLink = `https://www.youtube.com/watch?v=${video.id.videoId}`;
 
-          console.log(video.snippet)
+          console.log(video.snippet);
 
           titleHtml.innerHTML = title;
 
-          console.log(videoLink)
+          console.log(videoLink);
 
-          thumbnail.src = thumbnails.default.url
+          thumbnail.src = thumbnails.default.url;
 
           downloadAudio(video.id.videoId);
         });
 
-
         const downloadAudio = (videoId) => {
           const downloadUrl = `http://scpanel.hostycord.com:10009/download?link=https://www.youtube.com/watch?v=${videoId}`;
-        
+
           axios
-            .get(downloadUrl, { responseType: 'arraybuffer' })
+            .get(downloadUrl, { responseType: "arraybuffer" })
             .then((response) => {
               createAndDownloadBlob(response.data);
             })
             .catch((error) => {
-              console.error('Error downloading video:', error);
+              console.error("Error downloading video:", error);
             });
-        };        
+        };
 
         const createAndDownloadBlob = (data) => {
-          const blob = new Blob([data], { type: 'audio/mp3' });
+          const blob = new Blob([data], { type: "audio/mp3" });
           const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.style.display = 'none';
+          const a = document.createElement("a");
+          a.style.display = "none";
           a.href = url;
-          a.download = 'video.mp4';
+          a.download = "audio.mp3";
           document.body.appendChild(a);
           a.click();
           window.URL.revokeObjectURL(url);
         };
 
-        
         setIsLoading(false);
       })
       .catch((error) => {
@@ -77,116 +74,99 @@ const Audio = () => {
 
   return (
     <>
-    <nav className="flex ml-5" aria-label="Breadcrumb">
-  <ol className="inline-flex items-center space-x-1 md:space-x-3">
-    <li className="inline-flex items-center">
-      <Link to={"/dashboard"} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-        <svg aria-hidden="true" className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-        Dashboard
-      </Link>
-    </li>
-   
-    <li aria-current="page">
-      <div className="flex items-center">
-        <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Audio</span>
-      </div>
-    </li>
-  </ol>
-</nav>
-    <div className="flex dark:bg-gray-700">
-      <div className="w-1/2 p-6 border-r border-gray-300 dark:border-gray-600">
-        <div className="mb-6">
-          <label
-            htmlFor="default-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Audio Prompt or Link
-          </label>
-          <input
-            type="text"
-            id="default-input"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <button
-            disabled={isLoading}
-            type="button"
-            onClick={handleDownloadClick}
-            className="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
-          >
-            {!isLoading ? (
-              "Download an audio"
-            ) : (
-              <>
-                <svg
-                  aria-hidden="true"
-                  role="status"
-                  className="inline w-4 h-4 mr-3 text-white animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="#E5E7EB"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentColor"
-                  />
-                </svg>
-                Loading...
-              </>
-            )}
-          </button>
-        </div>
-      </div>
-      <div className="hidden w-1/2 p-6 flex flex-col gap-8 items-center justify-center">
-        <div
-          role="status"
-          className="flex w-full items-center justify-center h-56 max-w-sm bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700"
-        >
-          <svg
-            className="w-12 h-12 text-gray-200 dark:text-gray-600"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 384 512"
-          >
-            <path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-        <div role="status" className="animate-pulse">
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 max-w-[640px] mb-2.5 mx-auto"></div>
-          <div className="h-2.5 mx-auto bg-gray-300 rounded-full dark:bg-gray-700 max-w-[540px]"></div>
-          <div className="flex items-center justify-center mt-4">
+      <nav className="flex ml-5" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-3">
+          <li className="inline-flex items-center">
+            <Link
+              to={"/dashboard"}
+              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l1.293 1.293a1 1 0 001.414-1.414l-7-7zM18 11h-2a1 1 0 01-1-1V8a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1z" />
+              </svg>
+              Dashboard
+            </Link>
+          </li>
+          <li className="inline-flex items-center">
             <svg
-              className="w-10 h-10 mr-2 text-gray-200 dark:text-gray-700"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
+              className="w-3 h-3 mr-2 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                clipRule="evenodd"
-              ></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5l7 7-7 7"
+              />
             </svg>
-            <div className="w-20 h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 mr-3"></div>
-            <div className="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            <span className="text-sm font-medium text-gray-500">Audio</span>
+          </li>
+        </ol>
+      </nav>
+      <div className="mt-10 flex justify-center items-center">
+        <div className="w-full max-w-sm">
+          <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg px-4 py-6">
+            <h2 className="text-2xl font-bold mb-4">Audio Downloader</h2>
+            <div>
+              <input
+                type="text"
+                className="bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-700 rounded-lg py-2 px-4 block w-full focus:outline-none focus:border-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter video keywords"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </div>
+            <div className="mt-4">
+              <button
+                className="py-2 px-4 bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 text-white rounded-lg w-full focus:outline-none"
+                onClick={handleDownloadClick}
+              >
+                {isLoading ? "Loading..." : "Download"}
+              </button>
+            </div>
+            <div className="mt-4">
+              {isLoading && (
+                <div className="flex items-center">
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm10-2.647L21 20.938A7.962 7.962 0 0120 12h-4c0 3.042 1.135 5.824 3 7.938zM12 4.709L9 7.356V3.064a8.045 8.045 0 013 1.645z"
+                    />
+                  </svg>
+                  <span>Loading...</span>
+                </div>
+              )}
+            </div>
+            <div className="mt-4">
+              <img className="thumbnail" src="" alt="" />
+              <h3 className="title"></h3>
+            </div>
           </div>
-          <span className="sr-only">Loading...</span>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-8">
-        <img src="../../src/assets/audio.svg" className="ml-12" alt="Audio" />
-        <p className="mb-3 ml-12 text-center text-gray-500 dark:text-gray-400">
-          Make sure you provide a good name or a good link for your audio.
-        </p>
-      </div>
-    </div>
     </>
   );
 };
